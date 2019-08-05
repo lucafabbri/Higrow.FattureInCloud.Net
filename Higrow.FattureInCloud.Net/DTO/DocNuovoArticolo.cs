3f70b0425094575b592aeed54dd5ed1134d6377b
Luca Fabbri <luca@zepfiro.com>
2019-08-06 01:02:52 +0200

    double vals

--- Higrow.FattureInCloud.Net/DTO/DocNuovoArticolo.cs
+++ Higrow.FattureInCloud.Net/DTO/DocNuovoArticolo.cs
@@ -9,14 +9,14 @@
         public int quantita { get; set; }
         public string descrizione { get; set; }
         public string categoria { get; set; }
-        public int prezzo_netto { get; set; }
-        public int prezzo_lordo { get; set; }
+        public double prezzo_netto { get; set; }
+        public double prezzo_lordo { get; set; }
         public int cod_iva { get; set; }
         public bool tassabile { get; set; }
         public int sconto { get; set; }
         public bool applica_ra_contributi { get; set; }
         public int ordine { get; set; }
-        public int sconto_rosso { get; set; }
+        public string sconto_rosso { get; set; }
         public bool in_ddt { get; set; }
         public bool magazzino { get; set; }
     }
